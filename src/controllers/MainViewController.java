c0db8b39b9ef9a97e89d419543540de1e4c6b0fa
jbattilana <battilanajn@gmail.com>
2018-08-27 14:24:03 -0300

    Added support for horizontal split, need to add support if image.horizontal is not divisible by horizontal splits

--- src/controllers/MainViewController.java
+++ src/controllers/MainViewController.java
@@ -64,20 +64,24 @@ public class MainViewController implements Initializable {
             File auxFile = dChooser.showDialog(stage);
             for(File file:files) {
                 BufferedImage source = ImageIO.read(file);
-                int aux = source.getWidth()%Integer.parseInt(VerticalTextArea.getText();
-                int parseSize = source.getWidth()/Integer.parseInt(VerticalTextArea.getText());
+                int verticalExcess = source.getWidth()%Integer.parseInt(VerticalTextArea.getText();
+                int horizontalExcess = source.getHeight()%Integer.parseInt(HorizontalTextArea.getText());
+                int parseSizeVertical = source.getWidth()/Integer.parseInt(VerticalTextArea.getText());
+                int parseSizeHorizontal = source.getHeight()/Integer.parseInt(HorizontalTextArea.getText()));
                 if(aux==0) {
-                    ImageIO.write(source.getSubimage(0, 0, parseSize+aux, source.getHeight()), "png", new File(s.toString()))
+                    ImageIO.write(source.getSubimage(0, 0, parseSize+verticalExcess, source.getHeight()), "png", new File(s.toString()))
                 }
                 else{
-                    ImageIO.write(source.getSubimage(0, 0, parseSize+aux, source.getHeight()), "png", new File(s.toString()))
+                    ImageIO.write(source.getSubimage(0, 0, parseSize+verticalExcess, source.getHeight()), "png", new File(s.toString()))
                 }
                     for (int i = 1; i < Integer.parseInt(VerticalTextArea.getText());i++) {
-                        StringBuffer s = new StringBuffer();
-                        s.append(auxFile.getAbsolutePath());
-                        s.append("\\"+file.getName().split("\\.")[0]);
-                        s.append("_"+(Integer.parseInt(VerticalTextArea.getText())-i)+".png");
-                        ImageIO.write(source.getSubimage(i*parseSize+aux, 0, parseSize, source.getHeight()), "png", new File(s.toString()));
+                        for(int j= 1; j < Integer.parseInt(HorizontalTextArea.getText()));j++){
+                            StringBuffer s = new StringBuffer();
+                            s.append(auxFile.getAbsolutePath());
+                            s.append("\\" + file.getName().split("\\.")[0]);
+                            s.append("_" + (Integer.parseInt(VerticalTextArea.getText()+HorizontalTextArea.getText()) - i - j) + ".png");
+                            ImageIO.write(source.getSubimage(i * parseSizeVertical + verticalExcess, j * parseSizeHorizontal + horizontalExcess , parseSizeVertical, parseSizeHorizontal), "png", new File(s.toString()));
+                        }
                     }
             }
         }
